3ad42adc4b021e164f04ae5b3869d62d808d74b1
Milton Valencia R <miltonalonsovalenciarincon@gmail.com>
2026-07-03 10:43:23 -0500

    test: implementar suites de pruebas exhaustivas para los controladores de usuarios y tareas con vitest

--- src/tests/task.test.ts
+++ src/tests/task.test.ts
@@ -5,6 +5,7 @@ import { app } from "../index.ts";
 
 describe("🛡️ task controller", () => {
 	let tokenValid: string;
+	let taskIdCreated: number;
 
 	beforeAll(async () => {
 		try {
@@ -41,14 +42,58 @@ describe("🛡️ task controller", () => {
 				description: "Task 1 description",
 			});
 
+		taskIdCreated = response.body.task.id;
+
 		expect(response.status).toBe(201);
 		expect(response.body.message).toBe("✅ Tarea creada con éxito!");
 	});
 
-	/*test("Debería retornar status 200 y obtener todas las tareas", async () => {
-		const response = await request(app).get("/api/tasks");
+	test("debería retornar error 400 si no se envía el título de la tarea", async () => {
+		const response = await request(app)
+			.post("/api/tasks")
+			.set("Authorization", `Bearer ${tokenValid}`)
+			.send({
+				title: undefined,
+				description: undefined,
+			});
+		expect(response.status).toBe(400);
+		expect(response.body.error).toBe("⚠️ El título de la tarea es obligatorio.");
+	});
+
+	test("Debería retornar status 200 y obtener todas las tareas", async () => {
+		const response = await request(app)
+			.get("/api/tasks")
+			.set("Authorization", `Bearer ${tokenValid}`);
 
 		expect(response.status).toBe(200);
 		expect(response.body.message).toBe("✅ Tareas obtenidas con éxito!");
-	});*/
+		expect(response.body.tasks).toContainEqual(
+			expect.objectContaining({
+				title: "Task 1",
+				description: "Task 1 description",
+			}),
+		);
+	});
+
+	test("Debería actualizar la tarea correctamente", async () => {
+		const response = await request(app)
+			.put(`/api/tasks/${taskIdCreated}`)
+			.set("Authorization", `Bearer ${tokenValid}`)
+			.send({
+				title: "Task 1 updated",
+				description: "Task 1 description updated",
+			});
+
+		expect(response.status).toBe(200);
+		expect(response.body.message).toBe("✅ Tarea actualizada con éxito!");
+	});
+
+	test("Debería eliminar la tarea correctamente", async () => {
+		const response = await request(app)
+			.delete(`/api/tasks/${taskIdCreated}`)
+			.set("Authorization", `Bearer ${tokenValid}`);
+
+		expect(response.status).toBe(200);
+		expect(response.body.message).toBe("✅ Tarea eliminada con éxito!");
+	});
 });
